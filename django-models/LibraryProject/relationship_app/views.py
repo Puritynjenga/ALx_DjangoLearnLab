b825f66b2b14eb5e1b7636b02212958c515ae72d
Puritynjenga <puritynjenga895@gmail.com>
2025-07-20 16:51:16 +0300

    modified the views file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Author, Book, Librarian, Library
+from django.views.generic import DetailView
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    context= {
+        'books_list': books,  
+    }
+    return render(request, 'relationship_app/book_list.html', context)
+
+class LibraryView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_books.html'
+    context_object_name = 'library'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['books'] = self.object.books.all()
+    #     return context
+    def get_books_in_library(self):
+        #prefetch related books to avoid N+1 query problem
+        return Library.objects.prefetch_related('books')
+    
--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', views.books_list, name='books_list'),
+    path('library/<int:pk>/', views.LibraryView.as_view(), name='library-detail')
 ]
+
