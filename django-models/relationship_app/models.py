0c73bc8254a6a8950feea217fdfada2d7357e8c8
Puritynjenga <puritynjenga895@gmail.com>
2025-07-22 18:37:15 +0300

    added view to cater for user login, logout, register and authorization

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -34,7 +34,7 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name        
      
-class UserProfile(models.Model):
+class CreateUserProfile(models.Model):
     """Model representing a user profile."""
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=50, choices=[('librarian', 'Librarian'), ('member', 'Member'),('admin','Admin')], default='member')   
