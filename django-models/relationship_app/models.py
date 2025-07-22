7eb7bcd21f8a00f489b671d9146b02019f6df95a
Puritynjenga <puritynjenga895@gmail.com>
2025-07-22 19:12:54 +0300

    modified model.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -37,7 +37,7 @@ class Librarian(models.Model):
 class UserProfile(models.Model):
     """Model representing a user profile."""
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='profile')
-    role = models.CharField(max_length=50, choices=[('librarian', 'Librarian'), ('member', 'Member'),('admin','Admin')], default='member')   
+    role = models.CharField(max_length=50, choices=[('Librarian', 'Librarian'), ('Member', 'Member'),('Admin','Admin')], default='member')   
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
