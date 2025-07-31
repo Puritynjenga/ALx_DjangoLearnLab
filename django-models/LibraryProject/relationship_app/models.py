d1440a201514a551ef1b3ad6ed74bbfd676147f1
purity <puritynjenga895@gmail.com>
2025-07-31 10:36:53 +0300

    made changes in the relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,4 @@
+import django
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required, user_passes_test
@@ -9,11 +10,20 @@ class Author(models.Model):
     
     def __str__(self):
         return self.name
-    
+   
 class Book(models.Model):
     """Model representing a book."""
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_date = models.DateField()
+    class Meta:
+        # Custom permissions for the Book model
+    
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),]
+
     
     def __str__(self):
         return self.title   
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Library
 from .models import Book
 from django.views.generic.detail import DetailView
@@ -6,8 +6,9 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django .contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
@@ -73,4 +74,33 @@ def member_view(request):
     """View for member users."""
     return render(request, 'relationship_app/member_view.html')
 
-    
+@permission_required('relationship_app.can_add_book')
+def can_add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author =request.POST.get('author')
+        publication_date = request.POST.get('publication_date')
+        Book.objects.create(title=title, author=author, publication_date=publication_date)
+        messages.success(request, 'Book added successfully.')
+        return redirect('list_books')
+@permission_required('relationship_app.can_change_book') 
+def can_change_book(request,book_id):
+    if request.method == 'POST':
+        book = Book.objects.get(id=book_id)
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.publication_date = request.POST.get('publication_date')
+        book.save()
+        messages.success(request, 'Book updated successfully.')
+        return redirect('list_books')
+
+@permission_required('relationship_app.can_delete_book')
+def can_delete_book(request,book_id):
+    if request.method == 'POST':
+        book = Book.objects.get(id=book_id)
+        book.delete()
+        messages.success(request, 'Book deleted successfully.')
+        return redirect('list_books')
+    else:
+        messages.error(request, 'You do not have permission to delete this book.')
+        return redirect('list_books')
